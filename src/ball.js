a8281664acd473f0bd9cd0d2f0ea9ef195b53642
CatsAreMLG <joshuadgon@gmail.com>
2019-03-09 01:41:11 -0600

    Add paddle collision

--- src/ball.js
+++ src/ball.js
@@ -19,16 +19,32 @@ export default class Ball {
   update(deltaTime, { width, height }) {
     this.pos.x += this.speed.x
     this.pos.y += this.speed.y
+    //wall bottom
     if (this.pos.y >= height - this.radius) {
       this.pos.y = height / 2
       this.pos.x = width / 2
       this.speed = { x: Math.random() * 4 - 2, y: 2 }
     }
+    //wall top
     if (this.pos.y <= this.radius) {
       this.speed.y *= -1
     }
+    //wall left/right
     if (this.pos.x >= width - this.radius || this.pos.x <= 0 + this.radius) {
       this.speed.x *= -1
+    }
+    //paddle collision
+    let bottomOfBall = this.pos.y + this.radius
+    let topOfPaddle = this.game.paddle.pos.y
+    let leftOfPaddle = this.game.paddle.pos.x
+    let rightOfPaddle = this.game.paddle.pos.x + this.game.paddle.width
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.pos.x + this.radius >= leftOfPaddle &&
+      this.pos.x - this.radius <= rightOfPaddle
+    ) {
+      this.pos.y = topOfPaddle - this.radius
+      this.speed.y *= -1
     }
   }
 }
